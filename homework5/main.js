b339d20380126ede5f51557ab99238936b54a736
Alex1H <sasha.holovenko@gmail.com>
2022-04-14 18:39:12 +0300

    js - 1 corrected

--- homework5/main.js
+++ homework5/main.js
@@ -26,30 +26,40 @@
 //     authors: ['William M.', 'Brobeck L.'],
 // }
 
-let firstName = 'Oleksandr';
-let middleName ='Mykolayovych';
-let lastName = 'Holovenko';
-let person = firstName+" "+middleName+" "+lastName;
-console.log(person)
+// let firstName = 'Oleksandr';
+// let middleName ='Mykolayovych';
+// let lastName = 'Holovenko';
+// let person = firstName+" "+middleName+" "+lastName;
+// console.log(person)
 
 // prompt('Name', 'Oleksandr')
 // prompt('Surname', 'Holovenko')
 // prompt('age', 36)
 
-let a = 100;
-console.log(typeof a);
+let name = prompt('Oleksandr');
+let surname = prompt('Holovenko');
+let age = prompt('36');
+let person = name + surname + age;
+console.log(person);
 
-let b = '100';
-console.log(typeof b);
 
-let c = true;
-console.log(typeof c);
 
-let book = [
-    {name:'Спілка Рудих', genre:'детектив', pages:150, author:'Конан Дойл'},
-    {name:'Тарза', genre:'пригоди', pages:130, author:'Берроуз'},
-    {name:'Роксолана', genre:'роман', pages:230, author:'Загребельний'},
-]
+// let person = firstName+" "+middleName+" "+lastName;
 
-console.log(book[0]);
-console.log(book[2]);
+// let a = 100;
+// console.log(typeof a);
+//
+// let b = '100';
+// console.log(typeof b);
+//
+// let c = true;
+// console.log(typeof c);
+//
+// let book = [
+//     {name:'Спілка Рудих', genre:'детектив', pages:150, author:'Конан Дойл'},
+//     {name:'Тарза', genre:'пригоди', pages:130, author:'Берроуз'},
+//     {name:'Роксолана', genre:'роман', pages:230, author:'Загребельний'},
+// ]
+//
+// console.log(book[0]);
+// console.log(book[2]);
