ec5c4d86d512d151d537363459a2d6d869fa820e
Alex1H <sasha.holovenko@gmail.com>
2022-04-14 18:15:26 +0300

    js - 1

---
+++ homework5/main.js
@@ -0,0 +1,55 @@
+// let hello = 'hello'
+// // alert('hello');
+// document.write('hello');
+// console.log('hello');
+//
+// let owu = 'owu'
+// // alert('owu');
+// document.write('owu');
+// console.log('owu');
+//
+// let a = '3.14'
+// // alert('a');
+// document.write('a');
+// console.log(a);
+//
+// let book ={
+//     name: 'manual',
+//     pagenumber: 150,
+//     genre: 'science',
+// }
+//
+// let book2 ={
+//     name: 'manual',
+//     pagenumber: 200,
+//     genre: 'science',
+//     authors: ['William M.', 'Brobeck L.'],
+// }
+
+let firstName = 'Oleksandr';
+let middleName ='Mykolayovych';
+let lastName = 'Holovenko';
+let person = firstName+" "+middleName+" "+lastName;
+console.log(person)
+
+// prompt('Name', 'Oleksandr')
+// prompt('Surname', 'Holovenko')
+// prompt('age', 36)
+
+let a = 100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+let book = [
+    {name:'Спілка Рудих', genre:'детектив', pages:150, author:'Конан Дойл'},
+    {name:'Тарза', genre:'пригоди', pages:130, author:'Берроуз'},
+    {name:'Роксолана', genre:'роман', pages:230, author:'Загребельний'},
+]
+
+console.log(book[0]);
+console.log(book[2]);
