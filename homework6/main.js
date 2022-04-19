0d739185f405d4deb09b398abca5ab433035bea9
Alex1H <sasha.holovenko@gmail.com>
2022-04-19 16:20:11 +0300

    js - hw6

---
+++ homework6/main.js
@@ -0,0 +1,70 @@
+// let x=0;
+// let x=1;
+// let x=-3;
+//
+//  if (x!=0){
+//      document.write('Вірно')
+//  } else {
+//      document.write('Невіно')
+//  }
+
+
+
+// let a;
+// // a=5;
+// // a=17;
+// // a=31;
+// a=50;
+//
+// if (a > 0 && a <= 15) {
+//     document.write('first quarter')
+// }else if(a >15 && a<=30){
+//     document.write('second quarter')
+// }if (a >30 && a<=45){
+//     document.write('third quarter')
+// }else if(a >45 && a<=60){
+//     document.write('forth quarter')}
+
+
+
+// let day = 5;
+// switch (day) {
+//     case 1:
+//         document.write("Sunday");
+//         break;
+//     case 2:
+//         document.write("Monday");
+//         break;
+//     case 3:
+//         document.write("Tuesday");
+//         break;
+//     case 4:
+//         document.write("Wednesday");
+//         break;
+//     case 5:
+//         document.write("Thursday");
+//         break;
+//     case 6:
+//         document.write("Friday");
+//         break;
+//     case 7:
+//         document.write("Saturday");
+//         break;
+// }
+
+
+
+
+let a;
+let b;
+a=+prompt('the first number')
+b=+prompt('the second number')
+if (a>b){
+    document.write(a)
+}else if(a<b){
+    document.write(b)
+}else if(a===b){
+    document.write(a+' is equal to '+b)
+}else {
+    document.write('game is over')
+}
